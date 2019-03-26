f05915ad7ca5659420be426af60623adac9c5b0e
Nicolas Frankel <nicolas.frankel@exoscale.com>
2019-03-26 09:45:18 +0100

    Introduce configuration properties

--- src/main/java/com/exoscale/syslog/micronaut/MicronautConfig.java
+++ src/main/java/com/exoscale/syslog/micronaut/MicronautConfig.java
@@ -7,12 +7,12 @@ import org.springframework.context.annotation.Configuration;
 public class MicronautConfig {
 
     @Bean
-    public Foo foo() {
-        return new Foo();
+    public Foo foo(MicronautConfigProperties props) {
+        return new Foo(props);
     }
 
     @Bean
-    public Bar bar(Foo foo) {
-        return new Bar(foo);
+    public Bar bar(MicronautConfigProperties props, Foo foo) {
+        return new Bar(foo, props);
     }
 }
